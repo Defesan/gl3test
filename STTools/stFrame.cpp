c4729ab6f8a0fe4f8e00db4691fc48bc4c25927d
Jake Hopkins <defesan@gmail.com>
2017-06-20 20:50:39 -0600

    A good chunk of STFrame's functionality is in and compiling. Next comes the meat, which is mostly straightforward vector/matrix math.

--- STTools/stFrame.cpp
+++ STTools/stFrame.cpp
@@ -1,1 +1,100 @@
 #include "stFrame.h"
+
+STFrame::STFrame()
+{
+	this->origin = new STVec3f(0.0f, 0.0f, 0.0f);
+	this->upVector = new STVec3f(0.0f, 1.0f, 0.0f);
+	this->forwardVector = new STVec3f(0.0f, 0.0f, -1.0f);
+}
+
+STFrame::~STFrame()
+{
+
+
+}
+
+STVec3f* STFrame::getXAxis()
+{
+	STVec3f* result = new STVec3f();
+	
+	return result;
+}
+
+void STFrame::translateLocalBy(float x, float y, float z)
+{
+
+
+}
+
+STMatrix44f* STFrame::getMatrix(bool rotOnly)
+{
+	STMatrix44f* frameMatrix = new STMatrix44f();
+
+	return frameMatrix;
+}
+	
+	
+STMatrix44f* STFrame::getCameraMatrix(bool rotOnly)
+{
+	STMatrix44f* cameraMatrix = new STMatrix44f();
+
+	return cameraMatrix;
+}
+
+void STFrame::rotateLocalX(float angle)
+{
+
+}
+
+void STFrame::rotateLocalY(float angle)
+{
+
+}
+
+void STFrame::rotateLocalZ(float angle)
+{
+
+}
+	
+void STFrame::normalize()
+{
+
+}
+
+void STFrame::rotateWorld(float angle, float x, float y, float z)
+{
+
+}
+
+void STFrame::rotateLocal(float angle, float x, float y, float z)
+{
+
+}
+
+STVec3f* worldFromLocal(STVec3f* localVector, bool rotOnly)
+{
+	STVec3f* worldCoords = new STVec3f();
+	
+	return worldCoords;
+}
+
+STVec3f* localFromWorld(STVec3f* worldVector)
+{
+	STVec3f* localCoords = new STVec3f();
+
+	return localCoords;
+}
+	
+STVec3f* transformPoint(STVec3f* point)
+{
+	STVec3f* result = new STVec3f();
+	
+	return result;
+}
+
+STVec3f* rotateVector(STVec3f* vector)
+{
+	STVec3f* result = new STVec3f();
+	
+	return result;
+}
--- STTools/stFrame.h
+++ STTools/stFrame.h
@@ -8,15 +8,69 @@
 #include <cmath>
 #include <iostream>
 
+//This class defines the POV that the environment is seen through, in terms of an up vector, a forward vector, and an origin point.
+//Basically, it describes a translated plane in 3D coordinate space. This is combined with the frustum(which describes the volume of space visible to the frame)
+//and transformed by the model-view-projection matrix into two-dimensional screen coordinates.
+
 class STFrame
 {
 private:
-
-
+//So... There's only three vectors here. For some reason(?) RSW made these protected, but there's no hint that he ever derives a class from this one, so...
+	STVec3f* origin;
+	STVec3f* upVector;
+	STVec3f* forwardVector;
 
 public:
-
-
-
+	STFrame();	//Constructor.
+	~STFrame();
+	
+	void setOrigin(STVec3f* origin) {this->origin = origin;};
+	void setOrigin(float x, float y, float z) {this->origin = new STVec3f(x, y, z);};
+	
+	void setUp(STVec3f* upVector) {this->upVector = upVector;};
+	void setUp(float x, float y, float z) {this->upVector = new STVec3f(x, y, z);};
+	
+	void setForward(STVec3f* forwardVector) {this->forwardVector = forwardVector;};
+	void setForward(float x, float y, float z) {this->forwardVector = new STVec3f(x, y, z);};
+	
+	STVec3f* getOrigin() {return this->origin;};
+	STVec3f* getUp() {return this->upVector;};
+	STVec3f* getForward() {return this->forwardVector;};
+	
+	//These two are simple enough -- we HAVE these axes.
+	STVec3f* getYAxis() {return this->upVector;};
+	STVec3f* getZAxis() {return this->forwardVector;};
+	
+	//This one's a bit trickier, and I don't feel comfortable putting it in the header.
+	STVec3f* getXAxis();
+	
+	//Again, this is simple enough, and straightforward.
+	void translateWorldBy(float x, float y, float z) {this->origin->addVec3f(new STVec3f(x, y, z));};
+	
+	//But this...slightly trickier.
+	void translateLocalBy(float x, float y, float z);
+	
+	//Now all of the functions that most definitely don't fit in the header. Sorry, RSW, but I don't roll that way unless told to.
+	STMatrix44f* getMatrix(bool rotOnly);
+	STMatrix44f* getCameraMatrix(bool rotOnly);	//C++ no longer appreciates default parameters.
+	
+	void rotateLocalX(float angle);
+	void rotateLocalY(float angle);
+	void rotateLocalZ(float angle);
+	
+	void normalize();
+	
+	void rotateWorld(float angle, STVec3f* axis) {this->rotateWorld(angle, axis->getX(), axis->getY(), axis->getZ());};
+	void rotateWorld(float angle, float x, float y, float z);
+	
+	void rotateLocal(float angle, STVec3f* axis) {this->rotateLocal(angle, axis->getX(), axis->getY(), axis->getZ());};
+	void rotateLocal(float angle, float x, float y, float z);
+	
+	//Testing a theory postumated in the GLTools code...
+	STVec3f* worldFromLocal(STVec3f* localVector, bool rotOnly);
+	STVec3f* localFromWorld(STVec3f* worldVector);
+	
+	STVec3f* transformPoint(STVec3f* point);
+	STVec3f* rotateVector(STVec3f* vector);
 };
 #endif //__ST_FRAME_H__
