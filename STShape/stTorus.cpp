a7705a71ff66925644e57faf1086c40e344f8cb7
Jake Hopkins <defesan@gmail.com>
2017-07-17 21:32:28 -0600

    Think I have the indices worked out, and decided to copy the sphere's current all-black color scheme, which works well with a wireframe draw on a white background. Normals are next, even though I don't actually need them *quite* yet.

--- STShape/stTorus.cpp
+++ STShape/stTorus.cpp
@@ -92,15 +92,60 @@ void STTorus::genIndices()
 	//1, 3, 2
 	//And the second one goes:
 	//4, 2, 3
-	for(GLuint i = 0; i < this->verts.size() / 3; i++)
+	for(GLuint i = 0; i < this->numSections; i++)
 	{
-		int k = i * this->numSlices;
 		for(GLuint j = 0; j < this->numSlices; j++)
 		{
-			//So there's two sticking points here:
-			//1)when the inner loop wraps around(if j = numSlices - 1)
-			//2)when the outer loop wraps around(if i = numVerts - 1)
-			//In both these cases, adding one -- to find the next vertex in the loop or loop in the torus -- is...a bad idea.
+			//The vertex number SHOULD be i * numSlices + j
+			//In short, there are numSections * numSlices verts, so the whole thing could be viewed...as a two-dimensional array or matrix A(i,j)
+			int k = i * this->numSlices + j; 
+			int l = this->numSlices + j;
+			
+			int v1 = k;
+			int v2 = 0;
+			int v3 = 0;
+			int v4 = 0;
+			if(j < (this->numSlices - 1) && i < (this->numSections - 1))
+			{
+				//We're in the clear. The algorithm has no bumps.
+				v2 = k + 1;
+				v3 = k + l;
+				v4 = k + l + 1;
+			}
+			else if(j == (this->numSlices - 1) && i < (this->numSections - 1))
+			{
+				//We're not in the last section, but we are at the end of a section.
+				//This means that v2 is actually the first vertex in the section, and v4 is the first vertex of the NEXT section.
+				//However, v3 is unchanged.
+				v2 = k - j;
+				v3 = k + l;
+				v4 = k + l - j;
+			} 
+			else if(j < (this->numSlices - 1) && i == (this->numSections - 1))
+			{
+				//We're in the last section, but not at the end.
+				//This means that v2 is the next vertex in the section(k + 1),
+				//but v3 is the current vertex in the FIRST section, and v4 is the NEXT vertex in the FIRST section.
+				v2 = k + 1;
+				v3 = j;
+				v4 = j + 1;
+			}
+			else
+			{
+				//We're almost done, actually. This is the last triangle pair of the last section.
+				//v2, in this case, is the FIRST vertex of the LAST section, v3 is the LAST vertex of the FIRST section, and v4 is the FIRST vertex of the FIRST section.
+				//Okay, the caps got out of hand there...
+				v2 = k - j;
+				v3 = j;
+				v4 = 0;
+			}
+			
+			this->indexData.push_back(v1);
+			this->indexData.push_back(v3);
+			this->indexData.push_back(v2);
+			this->indexData.push_back(v4);
+			this->indexData.push_back(v2);
+			this->indexData.push_back(v3);
 		}		
 	}
 	this->batch->copyIndexData(this->indices);
@@ -108,7 +153,15 @@ void STTorus::genIndices()
 //...Color theory?
 void STTorus::genColors()
 {
-	//TODO
+	//Paint it black
+	for(GLuint i = 0; i < this->verts.size() / 3; i++)
+	{
+		this->colors.push_back(0.0f);
+		this->colors.push_back(0.0f);
+		this->colors.push_back(0.0f);
+		this->colors.push_back(1.0f);
+	
+	}
 	this->batch->copyColorData(this->colors);
 }
 //Linear algebra
